4b1520d72a97727877575fd6335c7256b700602f
gaurav <gaurav@connectionloops.com>
2025-11-09 08:37:21 -0800

    GetCronExpression util method

---
+++ util.cs
@@ -0,0 +1,27 @@
+using Cronos;
+
+namespace CLOOPS.microservices;
+
+/// <summary>
+/// Contains utility functions for the application
+/// </summary>
+public class Util : CLOOPS.NATS.Util
+{
+    /// <summary>
+    /// Parses a cron expression and returns a CronExpression object
+    /// </summary>
+    /// <param name="cron">The cron expression to parse</param>
+    /// <returns>A CronExpression object</returns>
+    /// <exception cref="Exception">Thrown if the cron expression is invalid</exception>
+    public static CronExpression GetCronExpression(string cron)
+    {
+        var mode = cron.Split(" ").Count() == 5 ? CronFormat.Standard : CronFormat.IncludeSeconds;
+        var cronExpression = CronExpression.Parse(cron, mode);
+        if (cronExpression is null)
+        {
+            throw new Exception($"Invalid cron expression: {cron}");
+        }
+        return cronExpression;
+    }
+
+}
